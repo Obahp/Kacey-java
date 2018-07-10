dd87332e473147d06c62027249aab3f02c78147d
Obah Prince Kelechi <Kelejoprince@gmail.com>
2018-07-10 09:17:01 +0100

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loops;
+import java.util.*;
+/**
+ *
+ * @author ONIKAN YOUTHCENTRE4
+ */
+public class Loops {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int count = 1; count < 10; count++) {
+         System.out.println(count);
+        }
+        
+    }
+    
+}
